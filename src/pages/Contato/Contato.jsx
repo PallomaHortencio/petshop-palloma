c27e9cda530767f58564568869d9a1c7d716bd67
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-20 10:47:58 -0300

    Update Contato.jsx

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -4,6 +4,11 @@ const Contato = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Pets Contato</h2>
+      <div id="contato">
+        <p>
+          <i>Em breve um formulário com biblioteca de componentes... </i>
+        </p>
+      </div>
     </section>
   );
 };
