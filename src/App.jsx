7ad0d15cadd0f105c90d4fe072b8b59a45d9c192
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-21 09:04:42 -0300

    Imagem 404

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home/Home";
 import Produtos from "./pages/Produtos/Produtos";
 import Sobre from "./pages/Sobre/Sobre";
 import Contato from "./pages/Contato/Contato";
-import Pagina404 from "./pages/Pagina404/Pagina404";
+import Pagina404 from "./pages/error/Pagina404";
 
 const App = () => {
   return (
@@ -31,7 +31,7 @@ const App = () => {
             </Route>
 
             <Route path="*">
-              <Pagina404></Pagina404>
+              <Pagina404 />
             </Route>
           </Switch>
         </main>
--- src/pages/Pagina404/Pagina404.jsx
+++
@@ -1,7 +0,0 @@
-import estilos from "./Pagina404.module.css";
-
-const Pagina404 = () => {
-  return <div>Ta certo!</div>;
-};
-
-export default Pagina404;
