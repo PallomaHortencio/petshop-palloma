143ecf621d3cac7fa9466f65b65d66bae52402fa
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-21 10:44:49 -0300

    att

--- src/components/Caixa/Caixa.jsx
+++ src/components/Caixa/Caixa.jsx
@@ -1,9 +1,18 @@
 import estilos from "./Caixa.module.css";
 
-const Caixa = (props) => {
+/* const Caixa = (props) => {
   return (
     <div className={`${estilos.caixa} ${props.listaDeClasses || ""}`}>
       {props.children}
+    </div>
+  );
+}; */
+
+/* Versão com props desestruturada */
+const Caixa = ({ children, listaDeClasses }) => {
+  return (
+    <div className={`${estilos.caixa} ${props.listaDeClasses || ""}`}>
+      {children}
     </div>
   );
 };
