c8dc7ad15e1ac4112f8ec4ec9eb503f1db08f070
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-21 09:55:39 -0300

    att

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,14 +1,15 @@
+import Caixa from "../../components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
 
 const Contato = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Pets Contato</h2>
-      <div id="contato">
+      <Caixa id="contato">
         <p>
           <i>Em breve um formulário com biblioteca de componentes... </i>
         </p>
-      </div>
+      </Caixa>
     </section>
   );
 };
