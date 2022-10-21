8cde45b2fe576a8d3be202062b990c792285ca89
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-10-21 11:51:19 -0300

    Update ListaPots.jsx

--- src/components/ListaPosts/ListaPots.jsx
+++ src/components/ListaPosts/ListaPots.jsx
@@ -31,7 +31,7 @@ const ListaPosts = () => {
   return (
     <div className={estilos.lista_posts}>
       {posts.map(({ id, titulo, subtitulo }) => (
-        <article className={estilos.post}>
+        <article className={estilos.post} key={id}>
           <h3>{titulo}</h3>
           <p>{subtitulo}</p>
         </article>
