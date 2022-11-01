16750750dd50053bd9ba7b1959544396e1baee91
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-11-01 09:28:11 -0300

    Ajustes da porta
    Para acessar pelo celular

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,5 +1,6 @@
 const porta = "2112";
 
-const serverApi = `http://localhost:${porta}`;
+/* const serverApi = `http://localhost:${porta}`; */ // Versão localhost
+const serverApi = `http://10.20.45.34:${porta}`; // Versão rede local via api
 
 export default serverApi;
